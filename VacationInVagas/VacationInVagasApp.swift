b7d4e97fd6efcfcd043f1874cf79bbd1ef9b7f48
Adarsh Shukla <adarshshukla10012002@gmail.com>
2024-11-04 22:39:37 +0530

    Initial Commit

---
+++ VacationInVagas/VacationInVagasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVagasApp.swift
+//  VacationInVagas
+//
+//  Created by adarsh shukla on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVagasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VacationInVagasTests/VacationInVagasTests.swift
@@ -0,0 +1,16 @@
+//
+//  VacationInVagasTests.swift
+//  VacationInVagasTests
+//
+//  Created by adarsh shukla on 04/11/24.
+//
+
+import Testing
+
+struct VacationInVagasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
